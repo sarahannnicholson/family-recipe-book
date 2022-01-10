fcab12f85077626b30d06958e486fa5ed0619e15
Sarah <a-sanic@microsoft.com>
2022-01-09 18:53:05 -0800

    Init - Adding MUI and swapping nav component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,6 +16,14 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
           rel="apple-touch-icon"
           sizes="180x180"
           href={`${withPrefix("/")}img/apple-touch-icon.png`}
