c2f196cf412f11b854af24f0349e7daef396b79c
Sarah <a-sanic@microsoft.com>
2022-01-09 20:18:13 -0800

    Deleting unneeded code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
